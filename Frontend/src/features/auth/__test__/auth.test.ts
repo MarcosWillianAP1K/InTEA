c8b7c94d270810b637c1d4bc0c397053bddbf214
Hermeson Alves <hermesonalves256@gmail.com>
2026-09-14 15:35:44 -0300

    feat: restructure frontend structure and add examples oud to use

---
+++ Frontend/src/features/auth/__test__/auth.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import { exemploService } from "../service/auth.service";
+
+describe("Frontend: ExemploService", () => {
+  it("deve listar os dados de exemplo", async () => {
+    const dados = await exemploService.listar();
+    expect(dados).toBeInstanceOf(Array);
+    expect(dados.length).toBeGreaterThan(0);
+    expect(dados[0]).toHaveProperty("id");
+    expect(dados[0]).toHaveProperty("titulo");
+  });
+});
